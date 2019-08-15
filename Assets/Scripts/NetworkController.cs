89c6f0fdf3c130097e8bc7d4fce5d132ff495949
grumblefart <52465526+grumblefart@users.noreply.github.com>
2019-08-15 11:07:29 -0400

    ONLINE WORKING BUT GITHUB DESKTOP BROKEN
    HEE HEE HOO HOO

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkController : MonoBehaviour
+{
+    string _room = "default";
+
+    void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("joined lobby");
+
+        RoomOptions roomOptions = new RoomOptions() { };
+        PhotonNetwork.JoinOrCreateRoom(_room, roomOptions, TypedLobby.Default);
+    }
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("im here");
+        PhotonNetwork.Instantiate("NetworkedPlayer", Vector3.zero, Quaternion.identity, 0);
+    }
+}
---
+++ Assets/Scripts/NetworkedPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// For use with Photon and SteamVR
+public class NetworkedPlayer : Photon.MonoBehaviour
+{
+    public GameObject avatar;
+
+    public Transform playerGlobal;
+    public Transform playerLocal;
+
+    void Awake()
+    {
+        Debug.Log("Player instantiated");
+
+        if (photonView.isMine)
+        {
+            Debug.Log("Player is mine");
+
+            playerGlobal = GameObject.Find("OVRPlayerController").transform;
+            playerLocal = playerGlobal.Find("OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+
+            this.transform.SetParent(playerLocal);
+            this.transform.localPosition = Vector3.zero;
+            this.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(playerGlobal.position);
+            stream.SendNext(playerGlobal.rotation);
+            stream.SendNext(playerLocal.localPosition);
+            stream.SendNext(playerLocal.localRotation);
+        }
+        else
+        {
+            this.transform.position = (Vector3)stream.ReceiveNext();
+            this.transform.rotation = (Quaternion)stream.ReceiveNext();
+            avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
+            avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
