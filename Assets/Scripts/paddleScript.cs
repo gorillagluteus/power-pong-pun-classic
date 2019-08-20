41713332eaf949d3d9f3f0712bfa84aefd635095
grumblefart <52465526+grumblefart@users.noreply.github.com>
2019-08-20 14:39:56 -0400

    paddle progress

---
+++ Assets/Scripts/paddleScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddleScript : MonoBehaviour
+{
+    public GameObject hand;
+    public Vector3 handPOffset;
+    public int[] handROffset = new int[3];
+    public Rigidbody rb;
+    public int sightLength = 20;
+    public float paddleForce;
+    private Vector3 point;
+
+    void Update()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(hand.transform.position, hand.transform.forward, 100.0F);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            if (hit.transform.tag == "hex")
+            {
+                point = hit.point;
+                break;
+            }
+        }
+        Debug.Log(point);
+        Vector3 direction = (point - transform.position).normalized;
+        rb.AddForce(direction * paddleForce);
+    }
+    // Update is called once per frame
+    void Awake()
+    {
+        point = GameObject.FindGameObjectWithTag("hex").transform.position;
+        rb = GetComponent<Rigidbody>();
+
+       string mainHand = "";
+        if (GameObject.FindGameObjectWithTag("GameController") != null)
+        {
+            mainHand = GameObject.FindGameObjectWithTag("GameController").transform.GetChild(0).GetComponent<localPlayerManager>().mainHand;
+        }
+        Debug.Log(mainHand);
+        hand = GameObject.FindGameObjectWithTag("lHand");
+        if (mainHand == "right")
+        {
+            hand = GameObject.FindGameObjectWithTag("rHand");
+        }
+    }
+}
